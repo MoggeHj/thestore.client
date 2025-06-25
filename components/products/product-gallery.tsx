728d714519c15b18beb76a6549764e5320d1acde
Morgan HJELM <morgan.hjelm@soprasteria.com>
2025-06-25 13:37:55 +0200

    product details, css changes on product gallery

--- components/products/product-gallery.tsx
+++ components/products/product-gallery.tsx
@@ -1,4 +1,5 @@
 "use client";
+"@heroicons/react/24/outline";
 import { useState } from "react";
 import Image from "next/image";
 
@@ -11,7 +12,7 @@ export default function ProductGallery({
 }) {
   const [selectedIdx, setSelectedIdx] = useState(0);
   const [startIdx, setStartIdx] = useState(0);
-  const maxThumbnails = 3;
+  const maxThumbnails = 4;
   const canScrollLeft = startIdx > 0 && imageUrls.length > maxThumbnails;
   const canScrollRight =
     imageUrls.length > maxThumbnails &&
@@ -19,7 +20,7 @@ export default function ProductGallery({
   const visibleThumbnails = imageUrls.slice(startIdx, startIdx + maxThumbnails);
 
   return (
-    <div className="grid md:grid-cols-5 gap-3 relative">
+    <div className="grid md:grid-cols-5 gap-3">
       <div className="md:col-span-10">
         <Image
           alt={name}
@@ -28,9 +29,8 @@ export default function ProductGallery({
           height={350}
           width={350}
         />
-
-        {imageUrls.length > maxThumbnails && (
-          <div className="flex items-center gap-2 justify-center absolute left-1/2 -translate-x-1/2 -bottom-12 z-10">
+        <div className="flex items-center gap-2 justify-center mt-4">
+          {imageUrls.length > maxThumbnails && (
             <button
               className="p-2 bg-white rounded-full shadow border mr-2 disabled:opacity-30"
               onClick={() => setStartIdx(startIdx - 1)}
@@ -39,26 +39,28 @@ export default function ProductGallery({
             >
               &#8592;
             </button>
-            {visibleThumbnails.map((url: string, idx: number) => (
-              <button
-                key={url + idx + startIdx}
-                onClick={() => setSelectedIdx(startIdx + idx)}
-                aria-label={`Show image ${startIdx + idx + 1}`}
-                className={
-                  (selectedIdx === startIdx + idx
-                    ? "ring-2 ring-blue-500 "
-                    : "") + "rounded"
-                }
-              >
-                <Image
-                  alt={name + " thumbnail " + (startIdx + idx + 1)}
-                  src={url}
-                  width={100}
-                  height={100}
-                  className="rounded border object-cover shadow-md"
-                />
-              </button>
-            ))}
+          )}
+          {visibleThumbnails.map((url: string, idx: number) => (
+            <button
+              key={url + idx + startIdx}
+              onClick={() => setSelectedIdx(startIdx + idx)}
+              aria-label={`Show image ${startIdx + idx + 1}`}
+              className={
+                (selectedIdx === startIdx + idx
+                  ? "ring-2 ring-blue-500 "
+                  : "") + "rounded"
+              }
+            >
+              <Image
+                alt={name + " thumbnail " + (startIdx + idx + 1)}
+                src={url}
+                width={100}
+                height={100}
+                className="rounded border object-cover shadow-md"
+              />
+            </button>
+          ))}
+          {imageUrls.length > maxThumbnails && (
             <button
               className="p-2 bg-white rounded-full shadow border ml-2 disabled:opacity-30"
               onClick={() => setStartIdx(startIdx + 1)}
@@ -67,32 +69,8 @@ export default function ProductGallery({
             >
               &#8594;
             </button>
-          </div>
-        )}
-        {imageUrls.length <= maxThumbnails && (
-          <div className="flex items-center gap-2 justify-center absolute left-1/2 -translate-x-1/2 -bottom-12 z-10">
-            {visibleThumbnails.map((url: string, idx: number) => (
-              <button
-                key={url + idx + startIdx}
-                onClick={() => setSelectedIdx(startIdx + idx)}
-                aria-label={`Show image ${startIdx + idx + 1}`}
-                className={
-                  (selectedIdx === startIdx + idx
-                    ? "ring-2 ring-blue-500 "
-                    : "") + "rounded"
-                }
-              >
-                <Image
-                  alt={name + " thumbnail " + (startIdx + idx + 1)}
-                  src={url}
-                  width={100}
-                  height={100}
-                  className="rounded border object-cover shadow-md"
-                />
-              </button>
-            ))}
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
