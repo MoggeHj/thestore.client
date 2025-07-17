55a320f9b9fd5773167c57b1bb139782c3793a0b
Morgan HJELM <morgan.hjelm@soprasteria.com>
2025-07-17 12:27:21 +0200

    make all products available on host specific page

--- src/app/souvenirs/[hostId]/page.tsx
+++ src/app/souvenirs/[hostId]/page.tsx
@@ -1,8 +1,22 @@
+import ProductGrid from "@root/components/products/product-grid";
+
 export default function HostSouvenirsPage() {
   return (
-    <div>
-      <h1 className="text-black text-2xl font-bold">Host Souvenirs Page</h1>
-      <p>This page is for managing souvenirs for a specific host.</p>
-    </div>
+    <>
+      <section>
+        <h1 className="text-black text-2xl font-bold">
+          <p>This page is for managing souvenirs for a specific host.</p>
+        </h1>
+      </section>
+
+      <section className="flex flex-col items-center justify-center w-full space-y-4 px-4">
+        <h2 className="text-xl font-bold tracking-tighter sm:text-2xl md:text-3xl mb-8">
+          Popular Products
+        </h2>
+        <div className="w-full max-w-screen-lg mx-auto">
+          <ProductGrid />
+        </div>
+      </section>
+    </>
   );
 }
